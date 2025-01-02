f236d3f44f7445107aabf886e36e4769996c154a
Tim Mastny <tim.mastny@gmail.com>
2025-01-02 17:32:29 -0600

    shifting works both ways in prototype2

--- project1/prototype2.py
+++ project1/prototype2.py
@@ -1,3 +1,6 @@
+RSHIFT = 0
+LSHIFT = 1
+
 def reverse_byte(b):
     b = ((b & 0xF0) >> 4) | ((b & 0x0F) << 4)
     b = ((b & 0xCC) >> 2) | ((b & 0x33) << 2)
@@ -23,8 +26,16 @@ def right_mask(byte, index):
     mask = 0xFF >> (8 - index)
     return mask, byte & mask
 
-def shift_bytes(s, shift):
+def rshift_bytes(s, shift):
+    #       rev: ---543210 2b 1b 765-----
+    #     shift:   >> 2
+    for i in range(len(s) - 1, 0, -1):
+        s[i] = ((s[i - 1] << 8) | s[i]) >> shift
+        s[i] &= 0xff
 
+    s[0] >>= shift
+
+def lshift_bytes(s, shift):
     for i in range(len(s) - 1):
         s[i] = ((s[i] << 8) | s[i + 1]) << shift
         s[i] &= 0xff00
@@ -33,45 +44,75 @@ def shift_bytes(s, shift):
     s[len(s) - 1] <<= shift
     s[len(s) - 1] &= 0xff
 
-def set_ends(s, start_mask, end_mask):
-    print(bin(start_mask[0]), bin(start_mask[1]))
-    print(bin(end_mask[0]), bin(end_mask[1]))
+def shift_bytes(s, shift):
+    if shift[0] == RSHIFT:
+        rshift_bytes(s, shift[1])
+    elif shift[0] == LSHIFT:
+        lshift_bytes(s, shift[1])
+    else:
+        raise ValueError("Invalid shift direction")
 
+def set_ends(s, start_mask, end_mask):
     s[0] = ~start_mask[0] & s[0] | start_mask[1]
     s[len(s) - 1] = ~end_mask[0] & s[0] | end_mask[1]
 
-def print_array(s, part):
-    print(f"{part}: " + ", ".join([f"{b:08b}" for b in s]))
+def calc_shift(start, end):
+    # example 1: ---34567 b1 b2 012-----
+    #             ^start: 3        ^end : 27
+    #       rev: -----210 2b 1b 76543---
+    #     shift:   << 2
+
+    # example 1: -----567 b1 b2 012345---
+    #                 ^start: 5       ^end : 29
+    #       rev: ---543210 2b 1b 765-----
+    #     shift:   >> 2
+    left = start
+    right = 8 - end % 8
+
+    shift = right - left
+    direction = LSHIFT if shift > 0 else RSHIFT
+
+    return  direction, abs(shift)
+
+def print_array(s, part='a'):
+    print(f"{part:15}: " + ", ".join([f"{b:08b}" for b in s]))
 
 def reverse(s, start, end):
     start_mask = left_mask(s[0], start)
     end_mask = right_mask(s[len(s) - 1], 8 - end % 8)
 
-    s = [reverse_byte(b) for b in s]
+    for i in range(len(s)):
+        s[i] = reverse_byte(s[i])
 
     print_array(s, "reverse_byte")
 
     reverse_array(s, 0, len(s))
     print_array(s, "reverse_array")
 
-    shift_bytes(s, 2)
+
+    shift_bytes(s, calc_shift(start, end))
     print_array(s, "shift")
 
 
     set_ends(s, start_mask, end_mask)
     print_array(s, "mask")
 
-# TODO: have to shift other way :(
-s = [0b10001_011, 0b10100011, 0b11110111, 0b11011_010]
-#      01234 567    89012345    67890123    45678 9
-#            ^start                               ^end
+s0 = [0b10001_011, 0b10100011, 0b11110111, 0b11011_010]
+#       01234 567    89012345    67890123    45678 9
+#             ^start                               ^end
+e0 = [0b10001_110, 0b11111011, 0b11110001, 0b01110_010]
+# reverse_byte   :   11010001, 11000101, 11101111, 01011011
+# reverse_array  :    01011011,  11101111,  11000101,  110_10001
+# shift          :  0001011011   10111111   11000101   110_100
+# mask           :  10001110, 11101111, 11110001, 10001010
 
-s = [0b010_11011, 0b10100011, 0b11110111, 0b100_01011]
+
+s1 = [0b010_11011, 0b10100011, 0b11110111, 0b100_01011]
 #      012 34567    89012345    67890123    456 789
 #          ^start                               ^end
 #    010_11011, 10100011, 11110111, 100_01011
 #    010_00111  11101111  11000101  11011_010
-
+e1 = [0b010_00111, 0b10111111, 0b00010111, 0b010_01011]
 
 #reverse_byte:   11011010, 11000101, 11101111, 11010001
 # reverse_array: 11010001, 11101111, 11000101, 11011010
@@ -80,6 +121,11 @@ s = [0b010_11011, 0b10100011, 0b11110111, 0b100_01011]
 #        ---                              -----
 # mask:  01000111, 10111111, 00010111, 01001011
 
+reverse(s0, 5, 29)
+s0 == e0
 
-
-reverse(s, 3, 27)
+# reverse(s1, 3, 27)
+# s1 == e1
+# reverse_array  :   010_11011, 11101111, 11000101, 110_10001
+# shift          : 00010_11011, 11101111, 11000101, 110_100
+# mask           : 10001_110, 11111011, 11110001, 10001_010
